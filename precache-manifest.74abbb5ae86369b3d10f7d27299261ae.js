5bf26e6274627b08077f1b1f73453a366fd1c354
Colin McCulloch <colinmucculloch@gmail.com>
2020-04-01 09:41:18 -0400

    Updates

---
+++ precache-manifest.74abbb5ae86369b3d10f7d27299261ae.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b97a7f54e6f5e7c7aa39ccf02eba97",
+    "url": "/restaurantMenu/index.html"
+  },
+  {
+    "revision": "46c5730f8e28335c26ea",
+    "url": "/restaurantMenu/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "61419d568b90bfaa65d3",
+    "url": "/restaurantMenu/static/css/main.f23a9c9d.chunk.css"
+  },
+  {
+    "revision": "46c5730f8e28335c26ea",
+    "url": "/restaurantMenu/static/js/2.5b6b576e.chunk.js"
+  },
+  {
+    "revision": "39ee42a9c6b75cfef987502f097de4c8",
+    "url": "/restaurantMenu/static/js/2.5b6b576e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61419d568b90bfaa65d3",
+    "url": "/restaurantMenu/static/js/main.97d8ed1d.chunk.js"
+  },
+  {
+    "revision": "24bb439b9824756e5310",
+    "url": "/restaurantMenu/static/js/runtime-main.6ecfc43f.js"
+  },
+  {
+    "revision": "8451c35bef7e7c8c1c15e2e87493b145",
+    "url": "/restaurantMenu/static/media/app.8451c35b.jpeg"
+  },
+  {
+    "revision": "113fb0385df63cd1eb8d205c33a5f94c",
+    "url": "/restaurantMenu/static/media/chickenFriedSteak.113fb038.jpg"
+  },
+  {
+    "revision": "456ca13e1e8f8b8f6aaded68ad71d5fd",
+    "url": "/restaurantMenu/static/media/cobbSalad.456ca13e.jpg"
+  },
+  {
+    "revision": "a02bd3b2927015a30e47f23ce251b14f",
+    "url": "/restaurantMenu/static/media/dessert.a02bd3b2.jpg"
+  },
+  {
+    "revision": "07883382e0f3927bf4340912389a067a",
+    "url": "/restaurantMenu/static/media/kids.07883382.jpg"
+  },
+  {
+    "revision": "2c32c9ac978383e0204c3de8a9df3a7d",
+    "url": "/restaurantMenu/static/media/turkey.2c32c9ac.jpeg"
+  }
+]);
